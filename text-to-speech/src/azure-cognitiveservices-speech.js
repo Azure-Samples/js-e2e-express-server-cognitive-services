b56126f6138e79a29581f96337e144bd87a469ef
Dina Berry <41597107+diberry@users.noreply.github.com>
2021-01-20 10:25:06 -0800

    comments (#4)

--- text-to-speech/src/azure-cognitiveservices-speech.js
+++ text-to-speech/src/azure-cognitiveservices-speech.js
@@ -1,9 +1,12 @@
+// azure-cognitiveservices-speech.js
+
 const sdk = require('microsoft-cognitiveservices-speech-sdk');
 const { Buffer } = require('buffer');
 const { PassThrough } = require('stream');
 const fs = require('fs');
 
 /**
+ * Node.js server code to convert text to speech
  * @returns stream
  * @param {*} key your resource key
  * @param {*} region your resource region
@@ -12,6 +15,7 @@ const fs = require('fs');
  */
 const textToSpeech = async (key, region, text, filename)=> {
     
+    // convert callback function to promise
     return new Promise((resolve, reject) => {
         
         const speechConfig = sdk.SpeechConfig.fromSubscription(key, region);
@@ -37,7 +41,9 @@ const textToSpeech = async (key, region, text, filename)=> {
                     const audioFile = fs.createReadStream(filename);
                     synthesizer.close();
                     resolve(audioFile);
+                    
                 } else {
+                    
                     // return stream from memory
                     const bufferStream = new PassThrough();
                     bufferStream.end(Buffer.from(audioData));
